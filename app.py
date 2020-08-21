82ad2922d7418d12b3542bc589c50a626f5e9ed5
TanveerMittal <tanveer@mittals.us>
2020-08-21 17:54:22 -0500

    Added react starter code

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
-from flask import Flask
+from flask import Flask, request
 from utils import *
+import pandas as pd
 app = Flask("forecasting api")
 
 @app.route('/forecast')
 def forecast_price():
+    start_date = request.args.get('start_date')
+    end_date = request.args.get('end_date')
+    num_guests = request.args.get("num_guests")
+    num_bedrooms = request.args.get("num_bedrooms")
+    num_beds = request.args.get("num_beds")
+    zipcode = request.args.get("zipcde")
+    min_review = request.args.get("min_review")
     return {"hello":"world", "test": 2}
 
 @app.route('/update')
